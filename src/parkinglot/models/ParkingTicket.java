a271ef7632b339bb10dd8c1171a2ef48a2e3712b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-15 09:29:45 +0530

    Remove Receipt fields from Parking Ticket model

--- src/parkinglot/models/ParkingTicket.java
+++ src/parkinglot/models/ParkingTicket.java
@@ -6,21 +6,11 @@ public class ParkingTicket {
     private final int ticketID;
     private final int spotNumber;
     private final Date entryTime;
-    private Date exitTime;
-    private int fare;
 
     public ParkingTicket(int ticketID, int spotNumber, Date entryTime) {
         this.ticketID = ticketID;
         this.spotNumber = spotNumber;
         this.entryTime = entryTime;
-    }
-
-    public void setExitTime(Date exitTime) {
-        this.exitTime = exitTime;
-    }
-
-    public Date getExitTime() {
-        return exitTime;
     }
 
     public int getSpotNumber() {
@@ -29,14 +19,6 @@ public class ParkingTicket {
 
     public Date getEntryTime() {
         return entryTime;
-    }
-
-    public int getFare() {
-        return fare;
-    }
-
-    public void setFare(int fare) {
-        this.fare = fare;
     }
 
     public int getTicketID() {
